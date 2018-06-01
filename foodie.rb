128c2456a4cc401ea2e44b3dcad1cc9a3b24d0dd
Dan Bartlett <danbartlett@gmail.com>
2018-06-01 13:11:23 +0100

    Add IngredientQuantity
    * This object deals with an Ingredient and a quantity
      and using those two bits of data calculates the appropriate
      totals from the default amount in the CSV

--- foodie.rb
+++ foodie.rb
@@ -64,6 +64,54 @@ class Ingredient < Sequel::Model
   end
 end
 
+class IngredientQuantity
+  attr_reader :amount, :ingredient
+
+  def initialize(ingredient:, amount:)
+    @ingredient = ingredient
+    @amount = amount
+
+    if amount && amount != ingredient.weight
+      @multiplier = amount.to_i.fraction_of ingredient.weight
+    end
+  end
+
+  def calories
+    @ingredient.calories * @multiplier
+  end
+
+  def carbohydrate
+    @ingredient.carbohydrate * @multiplier
+  end
+
+  def sugar
+    @ingredient.sugar * @multiplier
+  end
+
+  def fat
+    @ingredient.fat * @multiplier
+  end
+
+  def fibre
+    @ingredient.fibre * @multiplier
+  end
+
+  def protein
+    @ingredient.protein * @multiplier
+  end
+
+  def summary
+    rows = []
+    rows << ['Calories', "#{calories} kcal"]
+    rows << ['Carbs', "#{carbohydrate}g"]
+    rows << ['Sugar', "#{sugar}g"]
+    rows << ['Fat', "#{fat}g"]
+    rows << ['Protein', "#{protein}g"]
+    rows << ['Fibre', "#{fibre}g"]
+    Terminal::Table.new rows: rows, title: "#{@amount}g of #{@ingredient.name}"
+  end
+end
+
 class Meal
   def initialize(*ingredients)
     @all_ingredients = []
