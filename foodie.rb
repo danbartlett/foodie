8669935e75e4a1a2f86687bf4c671d3aa97bcf6d
Dan Bartlett <danbartlett@gmail.com>
2018-06-02 13:05:22 +0100

    Extract Ingredient to its own file

--- foodie.rb
+++ foodie.rb
@@ -28,42 +28,13 @@ DB.create_table :ingredients do
   Float :fibre
 end
 
-class Ingredient < Sequel::Model
-  def validate
-    super
-    errors.add(:carbohydrate, "can't be empty") if carbohydrate.nil?
-    errors.add(:sugar, "can't be empty") if sugar.nil?
-    errors.add(:fibre, "can't be empty") if fibre.nil?
-  end
+require_relative './models/ingredient.rb'
 
-  def macros
-    rows = []
-    rows << ['Carbs', "#{carbohydrate_percentage.round}%"]
-    rows << ['Sugar', "#{sugar_percentage.round}%"]
-    rows << ['Fat', "#{fat_percentage.round}%"]
-    rows << ['Protein', "#{protein_percentage.round}%"]
-    rows << ['Fibre', "#{fibre_percentage.round}%"]
-    Terminal::Table.new rows: rows, title: self.name
-  end
+class IngredientNotFound < StandardError; end
 
-  def protein_percentage
-    self.protein * 4 / self.calories * 100
-  end
-
-  def carbohydrate_percentage
-    self.carbohydrate * 4 / self.calories * 100
-  end
-
-  def sugar_percentage
-    self.sugar * 4 / self.calories * 100
-  end
-
-  def fat_percentage
-    self.fat * 9 / self.calories * 100
-  end
-
-  def fibre_percentage
-    self.fibre * 9 / self.calories * 100
+class Numeric
+  def fraction_of(n)
+    self.to_f / n.to_f
   end
 end
 
---
+++ models/ingredient.rb
@@ -0,0 +1,40 @@
+require 'sequel'
+
+class Ingredient < Sequel::Model
+  def validate
+    super
+    errors.add(:carbohydrate, "can't be empty") if carbohydrate.nil?
+    errors.add(:sugar, "can't be empty") if sugar.nil?
+    errors.add(:fibre, "can't be empty") if fibre.nil?
+  end
+
+  def macros
+    rows = []
+    rows << ['Carbs', "#{carbohydrate_percentage.round}%"]
+    rows << ['Sugar', "#{sugar_percentage.round}%"]
+    rows << ['Fat', "#{fat_percentage.round}%"]
+    rows << ['Protein', "#{protein_percentage.round}%"]
+    rows << ['Fibre', "#{fibre_percentage.round}%"]
+    Terminal::Table.new rows: rows, title: self.name
+  end
+
+  def protein_percentage
+    self.protein * 4 / self.calories * 100
+  end
+
+  def carbohydrate_percentage
+    self.carbohydrate * 4 / self.calories * 100
+  end
+
+  def sugar_percentage
+    self.sugar * 4 / self.calories * 100
+  end
+
+  def fat_percentage
+    self.fat * 9 / self.calories * 100
+  end
+
+  def fibre_percentage
+    self.fibre * 9 / self.calories * 100
+  end
+end
