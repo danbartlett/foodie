26320e4d3420768e84af83094e3110342256fe98
Dan Bartlett <danbartlett@gmail.com>
2018-06-02 15:56:40 +0100

    Remove duplicated methods from Ingredient; mixin module

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,4 +1,8 @@
+require_relative '../lib/macro_percentages'
+
 class Ingredient < Sequel::Model
+  include MacroPercentages
+
   def validate
     super
     errors.add(:carbohydrate, "can't be empty") if carbohydrate.nil?
@@ -14,25 +18,5 @@ class Ingredient < Sequel::Model
     rows << ['Protein', "#{protein_percentage.round}%"]
     rows << ['Fibre', "#{fibre_percentage.round}%"]
     Terminal::Table.new rows: rows, title: self.name
-  end
-
-  def protein_percentage
-    self.protein * 4 / self.calories * 100
-  end
-
-  def carbohydrate_percentage
-    self.carbohydrate * 4 / self.calories * 100
-  end
-
-  def sugar_percentage
-    self.sugar * 4 / self.calories * 100
-  end
-
-  def fat_percentage
-    self.fat * 9 / self.calories * 100
-  end
-
-  def fibre_percentage
-    self.fibre * 9 / self.calories * 100
   end
 end
