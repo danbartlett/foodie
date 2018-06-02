178fa3b93bf220aa36b0de66b1b758c11330a8fa
Dan Bartlett <danbartlett@gmail.com>
2018-06-02 15:43:25 +0100

    Remove duplicated macro methods in Meal; use module

--- models/meal.rb
+++ models/meal.rb
@@ -1,8 +1,11 @@
+require_relative '../lib/macros'
+
 class Meal
+  include Macros
   attr_reader :name
 
   def initialize(name:, ingredients:)
-    @all_ingredients = []
+    @foods = []
     @name = name
 
     ingredients.each do |ingredient|
@@ -10,57 +13,13 @@ class Meal
       ingredient_model = Ingredient.first(Sequel.like(:name, "#{name}%"))
       raise IngredientNotFound, "#{name} not found!" unless ingredient_model
 
-      @all_ingredients << Quantity.new(ingredient: ingredient_model, amount: amount)
+      @foods << Quantity.new(ingredient: ingredient_model, amount: amount)
     end
-  end
-
-  def calories
-    @all_ingredients.map(&:calories).sum
-  end
-
-  def carbohydrate
-    @all_ingredients.map(&:carbohydrate).sum
-  end
-
-  def sugar
-    @all_ingredients.map(&:sugar).sum
-  end
-
-  def fat
-    @all_ingredients.map(&:fat).sum
-  end
-
-  def fibre
-    @all_ingredients.map(&:fibre).sum
-  end
-
-  def protein
-    @all_ingredients.map(&:protein).sum
-  end
-
-  def protein_percentage
-    self.protein * 4 / self.calories * 100
-  end
-
-  def carbohydrate_percentage
-    self.carbohydrate * 4 / self.calories * 100
-  end
-
-  def sugar_percentage
-    self.sugar * 4 / self.calories * 100
-  end
-
-  def fat_percentage
-    self.fat * 9 / self.calories * 100
-  end
-
-  def fibre_percentage
-    self.fibre * 9 / self.calories * 100
   end
 
   def summary
     summary_table = Terminal::Table.new(title: "Ingredients for #{name}", headings: ['Ingredient', 'Calories', 'Carbs', 'Fat', 'Protein', 'Sugar', 'Fibre']) do |t|
-      @all_ingredients.each do |ingredient|
+      @foods.each do |ingredient|
         t.add_row [ingredient.to_s, ingredient.calories.round, "#{ingredient.carbohydrate.round}g", "#{ingredient.fat.round}g", "#{ingredient.protein.round}g", "#{ingredient.sugar.round}g", "#{ingredient.fibre.round}g"]
       end
       t.add_separator
