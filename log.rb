1ce7dda372330cb023b712778268dbbf04c36b17
Dan Bartlett <danbartlett@gmail.com>
2018-06-04 08:27:48 +0100

    Update log
    * Keeping this alive as I'm regularly truncating DB
      and don't want to loose any data

--- log.rb
+++ log.rb
@@ -1,48 +1,143 @@
-# FRIDAY, 01 JUN 2018
-# Porridge w/ Alpro, Banana & Almonds
-breakfast = Meal.new(name: 'Alpro Porridge w/ Banana & Almonds', ingredients: ['Porridge,50', 'Fage,50', 'Banana,1', 'Alpro Coconut,200', 'Almonds,30', 'Chia seeds,10'])
+def add_to_db(name:, ingredients:, date:)
+  meal = Meal.create(name: name, created_at: date)
+  ingredients.each do |ingredient|
+    name, quantity = ingredient.split(',')
+    ingredient_model = Ingredient.first(Sequel.like(:name, "#{name}%"))
+    raise IngredientNotFound, "#{name} not found!" unless ingredient_model
 
-# PWO Soreen and Choc Milk
-pre_wo_snack = Meal.new(name: 'Soreen & Choc Milk', ingredients: ['Choc milk,100', 'Soreen,2'])
+    meal.add_new ingredient_model, quantity
+  end
+end
 
-# Rego
-post_wo_snack = Meal.new(name: 'SIS Rego', ingredients: ['Rego,50'])
+today = Date.new(2018, 6, 1) # FRI 1 JUN
+add_to_db(
+  name: 'Alpro Porridge w/ Banana & Almonds',
+  ingredients: ['Porridge,50', 'Fage,50', 'Banana,1', 'Alpro Coconut,200', 'Almonds,30', 'Chia seeds,10'],
+  date: today
+)
+add_to_db(
+  name: 'PreWO Soreen & Choc Milk',
+  ingredients: ['Choc milk,100', 'Soreen,2'],
+  date: today
+)
+add_to_db(
+  name: 'PostWO SIS Rego',
+  ingredients: ['Rego,50'],
+  date: today
+)
+add_to_db(
+  name: 'Chilli on Sourdough',
+  ingredients: ['Sourdough,185', 'Chilli,360', 'Fage,50'],
+  date: today
+)
+add_to_db(
+  name: 'Huel & PB shake',
+  ingredients: ['Huel,112', 'Peanut butter,25', 'Choc shot,15', 'Alpro Coconut,100'],
+  date: today
+)
+add_to_db(
+  name: 'Biltong snack',
+  ingredients: ['Biltong,30'],
+  date: today
+)
+add_to_db(
+  name: 'Hot choc w/o milk',
+  ingredients: ['Hot choc,14'],
+  date: today
+)
+meals = Meal.where(created_at: today)
+day = Day.new(date: today, meals: meals)
+puts day.summary
 
-# Chilli on Sourdough
-lunch = Meal.new(name: 'Chilli on Sourdough', ingredients: ['Sourdough,185', 'Chilli,360', 'Fage,50'])
 
-dinner_shake = Meal.new(name: 'Huel & PB shake', ingredients: ['Huel,112', 'Peanut butter,25', 'Choc shot,15', 'Alpro Coconut,100'])
+today = Date.new(2018, 6, 2) # SAT 2 JUN
+add_to_db(
+  name: 'Scrambled eggs on toast',
+  ingredients: ['Eggs,147', 'Sourdough,90', 'Butter,6.5'],
+  date: today
+)
+add_to_db(
+  name: 'Two flat peaches',
+  ingredients: ['Flat peach,2'],
+  date: today
+)
+add_to_db(
+  name: 'Costa chicken sandwich and granola',
+  ingredients: ['Costa chicken salad sandwich,1', 'Costa granola square,1'],
+  date: today
+)
+add_to_db(
+  name: 'Lamb koftas, houmous & cous cous',
+  ingredients: ['Lamb koftas,120', 'Moroccan cous cous,150', 'Reduced fat houmous,60'],
+  date: today
+)
+add_to_db(
+  name: 'Hot choc',
+  ingredients: ['Hot choc,14'],
+  date: today
+)
+add_to_db(
+  name: 'BBQ belly & kebabs',
+  ingredients: ['Chicken kebab,4', 'Pork belly,200', 'Creamy coleslaw,75', 'Heineken,1'],
+  date: today
+)
+add_to_db(
+  name: 'Hot choc',
+  ingredients: ['Hot choc,14'],
+  date: today
+)
+meals = Meal.where(created_at: today)
+day = Day.new(date: today, meals: meals)
+puts day.summary
 
-biltong = Meal.new(name: 'Biltong snack', ingredients: ['Biltong,30'])
-hot_choc = Meal.new(name: 'Hot choc w/o milk', ingredients: ['Hot choc,14.5'])
+today = Date.new(2018, 6, 3) # SUN 3 JUN
+add_to_db(
+  name: 'LC breakfast',
+  ingredients: ['Fage,100', 'Almonds,50','Caramel rice cake,2'],
+  date: today
+)
+add_to_db(
+  name: 'Cous Cous & biltong',
+  ingredients: ['Reduced fat houmous,60', 'Moroccan cous cous,100', 'Biltong,30'],
+  date: today
+)
+add_to_db(
+  name: 'Nine bar!',
+  ingredients: ['Nine bar,1'],
+  date: today
+)
+add_to_db(
+  name: 'PreWO Chia Charge',
+  ingredients: ['Chia charge banana,1'],
+  date: today
+)
+add_to_db(
+  name: 'PostWO Banana & PB toast',
+  ingredients: ['Banana,1', 'Sourdough,66', 'Peanut butter,30'],
+  date: today
+)
+add_to_db(
+  name: 'Salmon & broc',
+  ingredients: ['Frozen salmon fillet,1', 'Tenderstem broccoli,100', 'Heineken,1'],
+  date: today
+)
+add_to_db(
+  name: 'Huel & snack',
+  ingredients: ['Huel,74','Flapjack,0.5', 'Hot choc,14'],
+  date: today
+)
+puts Meal.last.summary
+meals = Meal.where(created_at: today)
+day = Day.new(date: today, meals: meals)
+puts day.summary
 
-Day.new(breakfast, pre_wo_snack, post_wo_snack, lunch, dinner_shake, biltong, hot_choc)
-
-puts Ingredient.first.summary
-
-# SAT 02 JUN
-scrambled_eggs = Meal.new(name: 'Scrambled eggs on toast', ingredients: ['Eggs,147', 'Sourdough,90', 'Butter,6.5'])
-puts scrambled_eggs.summary
-
-peaches = Meal.new(name: 'Two flat peaches', ingredients: ['Flat peach,2'])
-puts peaches.summary
-
-costa = Meal.new(name: 'Costa chicken sandwich and granola', ingredients: ['Costa chicken salad sandwich,1', 'Costa granola square,1'])
-puts costa.summary
-
-lebanese_lunch = Meal.new(name: 'Lamb koftas, houmous & cous cous', ingredients: ['Lamb koftas,120', 'Moroccan cous cous,150', 'Reduced fat houmous,60'])
-puts lebanese_lunch.summary
-
-hot_choc = Meal.new(name: 'Hot choc', ingredients: ['Hot choc,14.5'])
-
-bbq = Meal.new(name: 'BBQ', ingredients: ['Chicken kebab,4', 'Pork belly,200', 'Creamy coleslaw,75', 'Heineken,1'])
-puts bbq.summary
-
-hot_choc_2 = Meal.new(name: 'Hot choc', ingredients: ['Hot choc,14.5'])
-saturday = Day.new(scrambled_eggs, peaches, costa, lebanese_lunch, hot_choc, bbq, hot_choc_2)
-puts saturday.summary
-
-test = Meal.new(name: 'Hot choc', ingredients: ['Fage,100', 'Almonds,50'])
-puts test.summary
-
-
+today = Date.new(2018, 6, 4) # MON 4 JUN
+add_to_db(
+  name: '4 egg scramble',
+  ingredients: ['Eggs,200', 'Sourdough,65', 'Butter,10'],
+  date: today
+)
+puts Meal.last.summary
+meals = Meal.where(created_at: today)
+day = Day.new(date: today, meals: meals)
+puts day.summary
