17a4def679b89a623c54c9fc041508307e936e41
Dan Bartlett <danbartlett@gmail.com>
2018-06-04 08:26:02 +0100

    Update Day to take a date

--- models/day.rb
+++ models/day.rb
@@ -5,13 +5,18 @@ class Day
   include MacroAggregates
   include MacroPercentages
 
-  attr_reader :foods
+  attr_reader :foods, :date
 
-  def initialize(*meals)
+  def initialize(date:, meals:)
     @foods = meals
+    @date = date
+  end
+
+  def meta
+    Meta.first(date: date)
   end
 
   def summary
-    SummaryTable.new(klass: self, title: "Meals for #{Time.now.strftime("%A, %b %d %Y")}").table
+    SummaryTable.new(klass: self, title: "Meals for #{date.strftime("%A, %b %d %Y")}").table
   end
 end
