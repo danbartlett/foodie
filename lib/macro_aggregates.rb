a29cee0801b783bbf91bd077fa805f6aa057c836
Dan Bartlett <danbartlett@gmail.com>
2018-06-04 08:22:13 +0100

    Use accessor, not instance var

--- lib/macro_aggregates.rb
+++ lib/macro_aggregates.rb
@@ -1,25 +1,25 @@
 module MacroAggregates
   def calories
-    @foods.map(&:calories).sum
+    foods.map(&:calories).sum
   end
 
   def carbohydrate
-    @foods.map(&:carbohydrate).sum
+    foods.map(&:carbohydrate).sum
   end
 
   def sugar
-    @foods.map(&:sugar).sum
+    foods.map(&:sugar).sum
   end
 
   def fat
-    @foods.map(&:fat).sum
+    foods.map(&:fat).sum
   end
 
   def fibre
-    @foods.map(&:fibre).sum
+    foods.map(&:fibre).sum
   end
 
   def protein
-    @foods.map(&:protein).sum
+    foods.map(&:protein).sum
   end
 end
