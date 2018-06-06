ffd11f1ceeca1639754bb267f15d012d283ffc63
Dan Bartlett <danbartlett@gmail.com>
2018-06-06 14:04:56 +0100

    New meals and updated titles

--- log.rb
+++ log.rb
@@ -34,13 +34,13 @@ add_to_db(
   date: today
 )
 add_to_db(
-  name: 'Huel & PB shake',
+  name: '3 Scoop Huel & PB shake',
   ingredients: ['Huel,112', 'Peanut butter,25', 'Choc shot,15', 'Alpro Coconut,100'],
   date: today
 )
 add_to_db(
   name: 'Biltong snack',
-  ingredients: ['Biltong,30'],
+  ingredients: ['Biltong,1'],
   date: today
 )
 add_to_db(
@@ -89,10 +89,10 @@ add_to_db(
   ingredients: ['Hot choc,14'],
   date: today
 )
-meals = Meal.where(created_at: today)
-Meta.create(date: today, garmin_total_calories: 2691, garmin_active_calories: 754, steps: 11242)
-day = Day.new(date: today, meals: meals)
-puts day.summary
+# meals = Meal.where(created_at: today)
+# Meta.create(date: today, garmin_total_calories: 2691, garmin_active_calories: 754, steps: 11242)
+# day = Day.new(date: today, meals: meals)
+# puts day.summary
 
 today = Date.new(2018, 6, 3) # SUN 3 JUN
 add_to_db(
@@ -102,7 +102,7 @@ add_to_db(
 )
 add_to_db(
   name: 'Cous Cous & biltong',
-  ingredients: ['Reduced fat houmous,60', 'Moroccan cous cous,100', 'Biltong,30'],
+  ingredients: ['Reduced fat houmous,60', 'Moroccan cous cous,100', 'Biltong,1'],
   date: today
 )
 add_to_db(
@@ -130,11 +130,10 @@ add_to_db(
   ingredients: ['Huel,74','Flapjack,0.5', 'Hot choc,14'],
   date: today
 )
-meals = Meal.where(created_at: today)
-Meta.create(date: today, garmin_total_calories: 2821, garmin_active_calories: 884, steps: 18402)
-day = Day.new(date: today, meals: meals)
-puts day.summary
-exit
+# meals = Meal.where(created_at: today)
+# Meta.create(date: today, garmin_total_calories: 2821, garmin_active_calories: 884, steps: 18402)
+# day = Day.new(date: today, meals: meals)
+# puts day.summary
 
 today = Date.new(2018, 6, 4) # MON 4 JUN
 add_to_db(
@@ -142,7 +141,77 @@ add_to_db(
   ingredients: ['Eggs,200', 'Sourdough,65', 'Butter,10'],
   date: today
 )
+add_to_db(
+  name: 'Chilli, sweet pot & salad',
+  ingredients: ['Chilli,300', 'Sweet potato,226', 'Omega seed mix,15'],
+  date: today
+)
+add_to_db(
+  name: 'PostWO snack',
+  ingredients: ['Flapjack,1','Coconut milk,150', 'Biltong,1', 'Apple,125'],
+  date: today
+)
+add_to_db(
+  name: 'Dins',
+  ingredients: ['Sausages,3', 'Reduced fat houmous,57', 'Sourdough,65', 'Butter,5'],
+  date: today
+)
+add_to_db(
+  name: 'Hot choc made completely with milk',
+  ingredients: ['Hot choc,14', 'Milk,200'],
+  date: today
+)
+# Resting: 1937
+meals = Meal.where(created_at: today)
+Meta.create(date: today, garmin_total_calories: 2727, garmin_active_calories: 790, steps: 16737)
+day = Day.new(date: today, meals: meals)
+puts day.summary
+
+today = Date.new(2018, 6, 5) # TUES 5 JUN
+add_to_db(
+  name: '3 scoop Huel w/ milk',
+  ingredients: ['Huel,112', 'Milk,200'],
+  date: today
+)
+add_to_db(
+  name: 'PreWO banana & flapjack',
+  ingredients: ['Flapjack,1', 'Banana,1'],
+  date: today
+)
+add_to_db(
+  name: 'Wagas & Picnic hot choc',
+  ingredients: ['Grilled tuna,1', 'Power juice,1', 'Picnic hot chocolate,1'],
+  date: today
+)
+add_to_db(
+  name: 'Cheese, oatcakes & apple',
+  ingredients: ['Oatcake,4', 'Apple,125', 'Cheese,20', 'Biltong,1'],
+  date: today
+)
+add_to_db(
+  name: 'Sliders, sweet pot & salad',
+  ingredients: ['Chorizo/chicken sliders,3', 'Omega seed mix,10', 'Sweet potato,161', 'Avocado,1', 'Tesco rice cake,2', 'Hot choc,14'], # Half av
+  date: today
+)
 puts Meal.last.summary
 meals = Meal.where(created_at: today)
+Meta.create(date: today, garmin_total_calories: 2711, garmin_active_calories: 774, steps: 16427)
+day = Day.new(date: today, meals: meals)
+puts day.summary
+
+today = Date.new(2018, 6, 6) # WEDS 6 JUN
+add_to_db(
+  name: '3 egg scrambled eggs on toast',
+  ingredients: ['Eggs,147', 'Sourdough,90', 'Butter,6.5'],
+  date: today
+)
+add_to_db(
+  name: 'Mackerel & Sweet Potato Salad',
+  ingredients: ['Sweet potato,274', 'Mackerel,141', 'Omega seed mix,10'],
+  date: today
+)
+puts Meal.last.summary
+meals = Meal.where(created_at: today)
+Meta.create(date: today, garmin_total_calories: 2711, garmin_active_calories: 774, steps: 16427)
 day = Day.new(date: today, meals: meals)
 puts day.summary
